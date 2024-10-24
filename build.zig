2a5d692663d7e2d280f57778713263a346638f9b
Robin Stumm <serverkorken@gmail.com>
2024-10-24 11:26:34 +0200

    move sources to new directory

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) !void {
 
     const exe = b.addExecutable(.{
         .name = "nix-sigstop",
-        .root_source_file = b.path("main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = opts.target,
         .optimize = opts.optimize,
     });
